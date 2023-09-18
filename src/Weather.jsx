278963bc87754e2cc3ff70cc1ad0871cf1463fbf
penszzip <pazhiloyflex@gmail.com>
2023-09-17 23:52:33 -0400

    final css tweaks

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -8,16 +8,16 @@ export default function Weather() {
     const [heading, setHeading] = useState("");
     const [icon, setIcon] = useState("");
     async function fetchForecast(location) {
-        
+
         const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}`);
         const coordinates = await response.json();
 
         const { latitude, longitude } = coordinates.results[0];
-        
+
         const responseWeather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
         const weather = await responseWeather.json();
         const { current_weather } = weather;
-        
+
         console.log(current_weather);
 
         setHeading(location);
@@ -27,16 +27,18 @@ export default function Weather() {
     }
 
     return (
-        <div>
-            <h1>WeatherApp</h1>
-            <WeatherForm dataFetcher={fetchForecast}/>
-            { forecast && 
-                <div className="weatherClass">
-                    { heading && <h2>{heading}</h2> }
-                    <h3>{icon}</h3>
-                    <p>Temperature: {forecast.temperature} degrees Celsius</p>
-                    <p>Wind speed: {forecast.windspeed} km/h</p>
-            </div> }
+        <div className="main">
+            <div className="wrapper">
+                <h1>WeatherApp</h1>
+                <WeatherForm dataFetcher={fetchForecast} />
+                {forecast &&
+                    <div className="weatherClass">
+                        {heading && <h2>{heading}</h2>}
+                        <h3>{icon}</h3>
+                        <p>Temperature: {forecast.temperature} degrees Celsius</p>
+                        <p>Wind speed: {forecast.windspeed} km/h</p>
+                    </div>}
+            </div>
         </div>
     )
 }
